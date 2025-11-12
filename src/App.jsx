9ee7c499badc041ff4f2aef38babc9b864f4da60
Ibraheem <abdulazeezibraheem68@gmail.com>
2025-11-11 20:00:30 -0800

    broke down the whole app to component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './component/navbar'
 import Hero from './component/Hero.jsx'
 import HomeCards from './component/HomeCards.jsx'
 import Joblistings from './component/Joblistings.jsx'
+import ViewAll from './component/ViewAll.jsx'
 
 const App = () => {
   return (
@@ -11,13 +12,7 @@ const App = () => {
      <Hero  title = "Become a React Dev" subtitle = "Find the React job that fits your skills and needs"/> 
      <HomeCards/>
      <Joblistings/>
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a>
-    </section>    
+     <ViewAll/>    
     </>
   )
 }
