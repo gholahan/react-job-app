80910f78986ef399be3090151cc6b9695186a2a1
Ibraheem <abdulazeezibraheem68@gmail.com>
2025-11-14 10:18:20 -0800

    a new age

--- src/Pages/JobsPage.jsx
+++ src/Pages/JobsPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Joblistings from '../component/Joblistings'
 
 const JobsPage = () => {
   return (
-    <div>JobsPage</div>
+    <section className='bg-blue-50 px-4 py-6 '>
+        <Joblistings showAll={true}/>
+    </section>
   )
 }
 
