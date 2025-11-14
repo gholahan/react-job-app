21946b21fa6b457b7a1e8776864ad4d8d82af777
Ibraheem <abdulazeezibraheem68@gmail.com>
2025-11-13 22:51:32 -0800

    react-router-dom changes a tag to Link tag

--- src/component/Joblisting.jsx
+++ src/component/Joblisting.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import {FaMapMarker}  from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Joblisting = ({job}) => {
 
@@ -31,12 +32,12 @@ const Joblisting = ({job}) => {
                   <FaMapMarker className="inline mr-2" />
                   {job.location}
                 </div>
-                <a
-                  href={`/jobs/${job.id}`}
+                <Link
+                  to={`/jobs/${job.id}`}
                   className="h-9 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
         </div>
